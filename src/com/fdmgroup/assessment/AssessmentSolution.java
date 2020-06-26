c449edcdbe14eead0349586965d758beef6d4a25
George Gichuki <george.gichuki@fdmgroup.com>
2020-06-26 15:20:38 -0500

    Initial commit

---
+++ src/com/fdmgroup/assessment/AssessmentSolution.java
@@ -0,0 +1,5 @@
+package com.fdmgroup.assessment;
+
+public class AssessmentSolution {
+
+}
---
+++ src/com/fdmgroup/assessment/Client.java
@@ -0,0 +1,10 @@
+package com.fdmgroup.assessment;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
