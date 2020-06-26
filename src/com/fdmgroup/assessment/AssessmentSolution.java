f8cea790f70110ac234f0b74b867a32266bd4b45
George Gichuki <george.gichuki@fdmgroup.com>
2020-06-26 17:29:37 -0500

    Final assessment solution

--- src/com/fdmgroup/assessment/AssessmentSolution.java
+++ src/com/fdmgroup/assessment/AssessmentSolution.java
@@ -1,5 +1,23 @@
 package com.fdmgroup.assessment;
 
 public class AssessmentSolution {
+	
+	private String string;
+	int myInt;
+	private static final char BREAK = '\n';
+	
+	public static String wrap(String string, int myInt){
+		
+		for(int i=0; i < string.length(); i++) {
+		
+		 return string.substring(0, myInt) + BREAK + string.substring(myInt);
+		 
+		
+		}
+		return string;
+		}
+		
+		
+	}
 
-}
+//My solution did not work. It breaks only the first time.
--- src/com/fdmgroup/assessment/Client.java
+++ src/com/fdmgroup/assessment/Client.java
@@ -3,8 +3,16 @@ package com.fdmgroup.assessment;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String str = "A hare is a fast animal";
+		int r = 3;
+		AssessmentSolution solution = new AssessmentSolution();
+		String st= solution.wrap(str, r);
+		System.out.println(st);		
 	}
-
 }
+
+	
+
+	
+
